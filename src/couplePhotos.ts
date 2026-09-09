a9e40dd0d2a7ca0d82442f399a14b64e7ec76eb9
matthagersenior <matthagersr@gmail.com>
2026-09-08 19:05:43 -0500

    feat: bundle supplied couple photos

---
+++ src/couplePhotos.ts
@@ -0,0 +1,17 @@
+import couplePhoto1 from "../download.jpeg";
+import couplePhoto2 from "../download (1).jpeg";
+import couplePhoto3 from "../download (2).jpeg";
+import couplePhoto4 from "../download (3).jpeg";
+import couplePhoto5 from "../download (4).jpeg";
+import couplePhoto6 from "../download (5).jpeg";
+
+export const COUPLE_PHOTOS = [
+  couplePhoto1,
+  couplePhoto2,
+  couplePhoto3,
+  couplePhoto4,
+  couplePhoto5,
+  couplePhoto6,
+];
+
+export { couplePhoto6 };
